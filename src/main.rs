ffa9f7ca6518a4b183524f7157b94072c99c6f7d
Fergus Yip <fergus@canva.com>
2023-06-01 08:33:56 +1000

    add commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate core;
+
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create pull requests for all branches in the current stack
+    Push {},
+    /// Update all pull requests in the current stack
+    Update {},
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Push {} => {
+            panic!("Push not implemented")
+        }
+        Commands::Update {} => {
+            panic!("Update not implemented")
+        }
+    }
 }
